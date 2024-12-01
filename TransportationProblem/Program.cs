87238738543d896e2066921f654b801ccf65c7c3
Bulat Shakirov <niiazych@gmail.com>
2024-12-01 19:35:07 +0000

    better sln

--- TransportationProblem/Program.cs
+++ TransportationProblem/Program.cs
@@ -6,93 +6,86 @@ using System.Threading.Tasks;
 class Program
 {
     static void Main(string[] args)
-    {
-        // Чтение данных из файла
-        var (supply, demand, costs) = ReadData("in.txt");
-        int n = supply.Length; // Количество поставщиков
-        int m = demand.Length; // Количество потребителей
+    {   
+        // Чтение входных данных
+        string[] inputLines = File.ReadAllLines("in.txt");
+        var firstLine = inputLines[0].Split(' ').Select(int.Parse).ToArray();
+        int N = firstLine[0]; // Количество поставщиков
+        int M = firstLine[1]; // Количество потребителей
 
-        // Начальное распределение
-        var allocation = new int[n, m];
-        NorthWestCornerMethod(supply, demand, costs, allocation);
+        int[] supplies = inputLines[1].Split(' ').Select(int.Parse).ToArray();
+        int[] demands = inputLines[2].Split(' ').Select(int.Parse).ToArray();
+        int[,] costs = new int[N, M];
 
-        OptimizeTransportation(supply, demand, costs, allocation);
-
-        // Запись результатов в файл, передаем costs
-        WriteResults("out.txt", allocation, costs);
-    }
-
-    static (int[] supply, int[] demand, int[,] costs) ReadData(string filePath)
-    {
-        using (var reader = new StreamReader(filePath))
+        for (int i = 0; i < N; i++)
         {
-            var firstLine = reader.ReadLine().Split(' ').Select(int.Parse).ToArray();
-            int n = firstLine[0], m = firstLine[1];
-    
-            var supply = reader.ReadLine().Split(' ').Select(int.Parse).ToArray();
-            var demand = reader.ReadLine().Split(' ').Select(int.Parse).ToArray();
-            var costs = new int[n, m];
-    
-            for (int i = 0; i < n; i++)
+            var costLine = inputLines[i + 3].Split(' ').Select(int.Parse).ToArray();
+            for (int j = 0; j < M; j++)
             {
-                var costLine = reader.ReadLine().Split(' ').Select(int.Parse).ToArray();
-                for (int j = 0; j < m; j++)
-                {
-                    costs[i, j] = costLine[j];
-                }
-            }
-    
-            return (supply, demand, costs);
-        }
-    }
-
-    static void NorthWestCornerMethod(int[] supply, int[] demand, int[,] costs, int[,] allocation)
-    {
-        int n = supply.Length;
-        int m = demand.Length;
-
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < m; j++)
-            {
-                if (supply[i] == 0 || demand[j] == 0) continue;
-
-                int amount = Math.Min(supply[i], demand[j]);
-                allocation[i, j] = amount;
-                supply[i] -= amount;
-                demand[j] -= amount;
+                costs[i, j] = costLine[j];
             }
         }
-    }
 
-    static void OptimizeTransportation(int[] supply, int[] demand, int[,] costs, int[,] allocation)
-    {
-        // Здесь можно реализовать метод оптимизации, например, метод потенциалов
-        // 
-    }
-        //TODO: вложенные циклы нехорошо
-    static void WriteResults(string filePath, int[,] allocation, int[,] costs)
-    {
-        using (var writer = new StreamWriter(filePath))
+        // Решение транспортной задачи
+        int[,] transports = new int[N, M];
+        SolveTransportationProblem(supplies, demands, costs, transports, N, M);
+
+        // Подсчет суммарных затрат
+        int totalCost = CalculateTotalCost(transports, costs, N, M);
+
+        // Запись результатов в файл
+        using (StreamWriter writer = new StreamWriter("out.txt"))
         {
-            int totalCost = 0;
-            for (int i = 0; i < allocation.GetLength(0); i++)
-            {
-                for (int j = 0; j < allocation.GetLength(1); j++)
-                {
-                    totalCost += allocation[i, j] * costs[i, j]; // Теперь costs доступен
-                }
-            }
             writer.WriteLine(totalCost);
-
-            for (int i = 0; i < allocation.GetLength(0); i++)
+            for (int i = 0; i < N; i++)
             {
-                for (int j = 0; j < allocation.GetLength(1); j++)
+                for (int j = 0; j < M; j++)
                 {
-                    writer.Write(allocation[i, j] + " ");
+                    writer.Write(transports[i, j] + (j < M - 1 ? " " : ""));
                 }
                 writer.WriteLine();
             }
         }
+    }
+
+    static void SolveTransportationProblem(int[] supplies, int[] demands, int[,] costs, int[,] transports, int N, int M)
+    {
+        int supplyIndex = 0, demandIndex = 0;
+
+        // Метод Северо-Западного Угла
+        while (supplyIndex < N && demandIndex < M)
+        {
+            int transportAmount = Math.Min(supplies[supplyIndex], demands[demandIndex]);
+            transports[supplyIndex, demandIndex] = transportAmount;
+            supplies[supplyIndex] -= transportAmount;
+            demands[demandIndex] -= transportAmount;
+
+            // Если поставщик исчерпан, переходим к следующему
+            if (supplies[supplyIndex] == 0)
+            {
+                supplyIndex++;
+            }
+
+            // Если потребитель исчерпан, переходим к следующему
+            if (demands[demandIndex] == 0)
+            {
+                demandIndex++;
+            }
+        }
+    }
+
+    static int CalculateTotalCost(int[,] transports, int[,] costs, int N, int M)
+    {
+        int totalCost = 0;
+
+        Parallel.For(0, N, i =>
+        {
+            for (int j = 0; j < M; j++)
+            {
+                totalCost += transports[i, j] * costs[i, j];
+            }
+        });
+
+        return totalCost;
     }
 }
